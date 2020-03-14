6ba8edb5f91ee69688cf2e67531d1e9636838116
liorjan <liorjan3@gmail.com>
2020-03-14 19:52:08 +0200

    step_05-14.3.20

--- app/src/main/java/com/example/petslife/MainActivity.java
+++ app/src/main/java/com/example/petslife/MainActivity.java
@@ -10,9 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.petslife.ProfileActivity;
-import com.example.petslife.R;
-
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private Object TextView;
--- app/src/main/java/com/example/petslife/ProfileActivity.java
+++ app/src/main/java/com/example/petslife/ProfileActivity.java
@@ -3,12 +3,26 @@ package com.example.petslife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button addPet;
+    String add_pet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button button = findViewById(R.id.add_pet);
+        addPet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),add_pet,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
